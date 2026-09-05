700d97263e2b42d49153447a4d0bc8d756a0698b
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 17:50:53 +0100

    Update constants.ts
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/sidebar/constants.ts
+++ src/sidebar/constants.ts
@@ -1,7 +1,4 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+
 
 export const NODE_KIND = 0;
 
