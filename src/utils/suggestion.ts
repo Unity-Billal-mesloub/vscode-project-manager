1e266455904dee60cda98aaa0bf781242ff14c9b
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 18:22:01 +0100

    Remove copyright header from suggestion.ts
    Removed copyright comment block from suggestion.ts
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/utils/suggestion.ts
+++ src/utils/suggestion.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
 
 import { commands, l10n, Uri, window, workspace } from "vscode";
 import path = require("path");
