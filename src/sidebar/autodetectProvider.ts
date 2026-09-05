43d6116f0be2c8a4fa3d25499ade948077b9edc6
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 17:49:57 +0100

    Remove copyright notice from autodetectProvider.ts
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/sidebar/autodetectProvider.ts
+++ src/sidebar/autodetectProvider.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
 
 import * as vscode from "vscode";
 import { CustomProjectLocator } from "../autodetect/abstractLocator";
