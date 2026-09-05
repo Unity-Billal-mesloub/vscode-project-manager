e0466688e3f3c1c7516463d8229585d3e5513815
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 19:08:10 +0100

    Remove copyright comments from webpack.config.js
    Removed copyright comments from the webpack configuration file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Alessandro Fragnani. All rights reserved.
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the GPLv3 License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 
 //@ts-check
 
