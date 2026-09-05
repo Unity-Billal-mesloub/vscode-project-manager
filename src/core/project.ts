ddb039a932e591cc852f039f69b430b2c48ff72e
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 17:45:21 +0100

    Remove copyright notice from project.ts
    Removed copyright notice from project.ts file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/core/project.ts
+++ src/core/project.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
 
 export interface Project {
     name: string;     // the name that the user defines for the project
