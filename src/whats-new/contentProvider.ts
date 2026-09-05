4dadee1f0cd8fb6efa431eecababff415c38ca9a
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 18:25:21 +0100

    Remove copyright comment from contentProvider.ts
    Removed copyright notice from contentProvider.ts
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/whats-new/contentProvider.ts
+++ src/whats-new/contentProvider.ts
@@ -1,7 +1,4 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+
 
 import { ChangeLogItem, ChangeLogKind, ContentProvider, Header, Image, IssueKind, SupportChannel, SocialMediaProvider } from "../../vscode-whats-new/src/ContentProvider";
 
