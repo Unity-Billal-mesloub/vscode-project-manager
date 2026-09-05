c9bf195506d04f7cff26a6c4fe16396a835edfca
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 17:47:33 +0100

    Remove copyright comment from projectsPicker.ts
    Removed copyright notice from projectsPicker.ts
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/quickpick/projectsPicker.ts
+++ src/quickpick/projectsPicker.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the GPLv3 License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
 
 import fs = require("fs");
 import { commands, Disposable, l10n, MessageItem, QuickInputButton, QuickPickItem, ThemeIcon, window, workspace } from "vscode";
